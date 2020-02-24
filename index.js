fc61a8e9e8298744f1b2c3f70bcfead2b9c2ee08
michielst <michieldegreef@outlook.com>
2020-02-24 20:44:28 +0100

    Remove headless false

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { login, tweet } = require('./twitter');
 
 (async () => {
   const tweet = process.argv[2];
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
 
   await login(page, user.email, user.password);
