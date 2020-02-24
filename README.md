64bd99d66b913a6a1474204327b89ec748b18786
michielst <michieldegreef@outlook.com>
2020-02-24 20:35:41 +0100

    Send tweet text as argument

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # twitter-puppeteer-bot
 Posting tweets using Puppeteer
+
+Start bot
+```
+$ npm install 
+$ npm start <tweet text>
+```
--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const user = require('./credentials.json');
 const { login, tweet } = require('./twitter');
 
 (async () => {
+  const tweet = process.argv[2];
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   await login(page, user.email, user.password);
-  await tweet(page, 'test');
+  await tweet(page, tweet);
   await browser.close();
 })();
